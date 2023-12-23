444b540d081896e2d71d3c4881c09a3fc866bd11
kanpurin <kanpurin2@gmail.com>
2023-12-23 14:13:51 +0900

    auto commit

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 if [ "$1" != "" ]; then
     cd "$1" || exit 1  # Change directory to RandomGenerator
-    git fetch origin   # Fetch from remote
-    git reset --hard   # Reset local changes
+    git fetch origin
+    git reset --hard "origin/$(git rev-parse --abbrev-ref HEAD)"
     cd ..              # Move back to the previous directory
     npm run copy "$1"  # Run npm script to copy files for RandomGenerator
     npm run git        # Run npm script to execute git-related tasks
