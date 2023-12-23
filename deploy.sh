23aacfc43d96d99ddf3749a59754645e2cc54e22
kanpurin <kanpurin2@gmail.com>
2023-12-23 14:36:45 +0900

    auto commit

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 if [ "$1" != "" ]; then
-    cd "$1" || exit 1  # Change directory to RandomGenerator
+    directory="$1"
+    cd "$directory" || exit 1  # Change directory to RandomGenerator
     git fetch origin
     git reset --hard "origin/$(git rev-parse --abbrev-ref HEAD)"
     cd ..              # Move back to the previous directory
-    npm run copy "$1"  # Run npm script to copy files for RandomGenerator
-    find docs -type f -exec sed -i 's/RandomGenerator/TestWebApp/g' {} +
-    # npm run git        # Run npm script to execute git-related tasks
+    npm run copy "$directory"  # Run npm script to copy files for RandomGenerator
+    find docs -type f -exec sed -i "s|$directory|TestWebApp|g" {} +  # Replace directory name with TestWebApp
+    npm run git        # Run npm script to execute git-related tasks
 else
     echo "Usage: $0 <directory>"
     exit 1
